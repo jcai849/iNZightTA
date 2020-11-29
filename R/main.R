f279230cb390482501170dde3b819c49edb98f31
Jason Cairns <jcai849@aucklanduni.ac.nz>
2020-11-30 09:56:00 +1300

    startup from anywhere

--- R/main.R
+++ R/main.R
@@ -3,5 +3,19 @@
 #' @return void; launch app for iNZightTA
 #'
 #' @export
-main <- function()
-    source(system.file("app", "app.R", package = "inzightta"))
+main <- function() {
+	if (!require(pacman)) install.packages("pacman")
+	devtools::install_github('charlie86/spotifyr', quiet=TRUE)
+	pacman::p_load(
+		       inzightta,
+		       shiny,
+		       ggplot2,
+		       data.table,
+		       purrr,
+		       quanteda
+		       )
+
+    source(system.file("app", "ui.R", package = "inzightta"))
+    source(system.file("app", "server.R", package = "inzightta"))
+    shiny::runApp(shiny::shinyApp(ui, server))
+}
